25af8e752ba64aaa76e52db5f2945e93a98b1c70
shoshaUg <stkaddofo@st.ug.edu.gh>
2025-03-14 13:07:03 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Lover Bot",
   botOwner: 'LOVER_BOY_BOT', // Your name
   ownerNumber: '233534268869', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "2.0.0",
 };
 
 module.exports = settings;
